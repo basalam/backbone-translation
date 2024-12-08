f58e3e677b6d5ad921f0b3ff64f72c6fb544fe6b
Asghari <mhmdasghari1@gmail.com>
2024-12-08 15:13:33 +0330

    feat: add setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-import setuptools
+from setuptools import setup, find_namespace_packages
 
-setuptools.setup(
+setup(
     name="backbone-translation",
     author="Mojtabaa Habibain",
     author_email="mojtabaa.hn@gmail.com",
     description="Python Utilities & Basalam Micro-Services SDK",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/basalam/backbone-translation",
+    packages=find_namespace_packages(where='src', include=['basalam.backbone_translation']),
+    package_dir={'': 'src'},
+    namespace_packages=["basalam"],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires='>=3.6',
     setuptools_git_versioning={"enabled": True},
     setup_requires=["setuptools-git-versioning"],
 )
